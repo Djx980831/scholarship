1fd8c8a7d61aacec1a4e32ec2b6a6063569e48ad
dongjingxiong <dongjingxiong@haodf.com>
2020-03-31 22:23:28 +0800

    增加redis配置

---
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,154 @@
+package com.example.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.nio.charset.Charset;
+
+/**
+ * redis 配置类
+ * @author： example
+ * @create: 2019/5/31
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.hostName}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private Integer port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private Integer timeout;
+
+    @Value("${spring.redis.maxIdle}")
+    private Integer maxIdle;
+
+    @Value("${spring.redis.maxActive}")
+    private String maxActive;
+
+    @Value("${spring.redis.maxTotal}")
+    private Integer maxTotal;
+
+    @Value("${spring.redis.maxWaitMillis}")
+    private Long maxWaitMillis;
+
+    @Value("${spring.redis.minEvictableIdleTimeMillis}")
+    private Long minEvictableIdleTimeMillis;
+
+    @Value("${spring.redis.numTestsPerEvictionRun}")
+    private Integer numTestsPerEvictionRun;
+
+    @Value("${spring.redis.timeBetweenEvictionRunsMillis}")
+    private Long timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.redis.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.redis.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.redis.database}")
+    private Integer database;
+
+    /**
+     * JedisPoolConfig 连接池
+     * @return
+     */
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        // 最大空闲数
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        // 连接池的最大数据库连接数
+        jedisPoolConfig.setMaxTotal(maxTotal);
+        // 最大建立连接等待时间
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 逐出连接的最小空闲时间 默认1800000毫秒(30分钟)
+        jedisPoolConfig.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        // 每次逐出检查时 逐出的最大数目 如果为负数就是 : 1/abs(n), 默认3
+        jedisPoolConfig.setNumTestsPerEvictionRun(numTestsPerEvictionRun);
+        // 逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
+        jedisPoolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        // 是否在从池中取出连接前进行检验,如果检验失败,则从池中去除连接并尝试取出另一个
+        jedisPoolConfig.setTestOnBorrow(testOnBorrow);
+        // 在空闲时检查有效性, 默认false
+        jedisPoolConfig.setTestWhileIdle(testWhileIdle);
+        return jedisPoolConfig;
+    }
+
+    /**
+     * jedis连接工厂
+     * @param jedisPoolConfig
+     * @return
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        //设置redis服务器的host或者ip地址
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        redisStandaloneConfiguration.setDatabase(database);
+        //获得默认的连接池构造
+        //这里需要注意的是，edisConnectionFactoryJ对于Standalone模式的没有（RedisStandaloneConfiguration，JedisPoolConfig）的构造函数，对此
+        //我们用JedisClientConfiguration接口的builder方法实例化一个构造器，还得类型转换
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jpcf = (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
+        //修改我们的连接池配置
+        jpcf.poolConfig(jedisPoolConfig);
+        //通过构造器来构造jedis客户端配置
+        JedisClientConfiguration jedisClientConfiguration = jpcf.build();
+        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+        return RedisCacheManager.create(connectionFactory);
+    }
+
+    /**
+     * 实例化 RedisTemplate
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> functionDomainRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        //设置value的序列化规则和 key的序列化规则
+        RedisSerializer stringSerializer = new StringRedisSerializer(Charset.forName("UTF8"));
+        redisTemplate.setKeySerializer(stringSerializer );
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer );
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
